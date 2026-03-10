269491071e10d91f32c21e9cd7867bf88ea0b7a0
Upshorelabs <dexexchange2025@gmail.com>
2026-03-10 19:08:22 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+ "name": "ai-flashloan-arbitrage-bot",
+ "version": "1.0.0",
+ "description": "AI Flash Loan Arbitrage Bot for BNB Smart Chain",
+ "main": "bot/agent.js",
+ "scripts": {
+  "start": "node bot/agent.js"
+ },
+ "dependencies": {
+  "ethers": "^6.7.0",
+  "axios": "^1.6.0",
+  "dotenv": "^16.4.0"
+ }
+}
